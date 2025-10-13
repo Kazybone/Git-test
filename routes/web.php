e8f09690b464a683f78e0822ce43a80ff689f587
Студенческая <@rbiu.local>
2025-10-13 11:12:33 +0500

    crud

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/reports', function () {
+    return view('report.index');
+})->name('reports.index');
+
+Route::get('/reports/create', function(){
+    return view('report.create');
+})->name('reports.create');
